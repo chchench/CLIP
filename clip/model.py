99e3d4f47f07d430c36852f6550ae4b2b72884e2
Charles Cheng <45220401+chchench@users.noreply.github.com>
2024-10-01 14:29:01 -0700

    Add Torch based implementation of UNet

--- clip/model.py
+++ clip/model.py
@@ -154,6 +154,62 @@ class ModifiedResNet(nn.Module):
         return x
 
 
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(UNet, self).__init__()
+        self.enc_conv1 = self.double_conv(n_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+        self.enc_conv2 = self.double_conv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+        self.enc_conv3 = self.double_conv(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+        self.enc_conv4 = self.double_conv(256, 512)
+        self.pool4 = nn.MaxPool2d(2)
+        self.bottleneck = self.double_conv(512, 1024)
+        self.upconv4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.dec_conv4 = self.double_conv(1024, 512)
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec_conv3 = self.double_conv(512, 256)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec_conv2 = self.double_conv(256, 128)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec_conv1 = self.double_conv(128, 64)
+        self.out_conv = nn.Conv2d(64, n_classes, kernel_size=1)
+
+    def double_conv(self, in_channels, out_channels):
+        conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        return conv
+
+    def forward(self, x):
+        # Encoder
+        enc1 = self.enc_conv1(x)
+        enc2 = self.enc_conv2(self.pool1(enc1))
+        enc3 = self.enc_conv3(self.pool2(enc2))
+        enc4 = self.enc_conv4(self.pool3(enc3))
+        # Bottleneck
+        bottleneck = self.bottleneck(self.pool4(enc4))
+        # Decoder
+        dec4 = self.upconv4(bottleneck)
+        dec4 = torch.cat((dec4, enc4), dim=1)
+        dec4 = self.dec_conv4(dec4)
+        dec3 = self.upconv3(dec4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.dec_conv3(dec3)
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.dec_conv2(dec2)
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.dec_conv1(dec1)
+        out = self.out_conv(dec1)
+        return out
+
+
 class LayerNorm(nn.LayerNorm):
     """Subclass torch's LayerNorm to handle fp16."""
 
